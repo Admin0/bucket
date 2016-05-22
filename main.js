16b5cf9462128752f7657cc70eab85469de22c15
Admin0 <givemebigpie@naver.com>
2016-05-22 14:45:29 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 function nav_create() {
     $("body").prepend("<nav></nav>");
     // $("nav").prepend("<section id='in-page'></section>");
-    $("h2,h3,h4").each(function(){
+    $("h2,h3").each(function(){
         $(this).clone()
         .html("<a href='#"+$(this).attr("id")+"'>"+$(this).text()+"</a>")
         .attr("id",null)
@@ -48,7 +48,7 @@ function scroll_smooth() {
 
             $('html, body').animate({
                 scrollTop: target_position
-            }, 300, function() {
+            }, 500, function() {
                 bg_change(target, target_bg, ".75s");
             });
         }
@@ -68,6 +68,47 @@ function scroll_smooth() {
     });
 }
 
+color = {
+    "length":15,
+    "material_50":[
+        "FFEBEE", "FCE4EC", "F3E5F5", "EDE7F6", "E8EAF6",
+        "E3F2FD", "E1F5FE", "E0F7FA", "E0F2F1", "E8F5E9",
+        "F1F8E9", "F9FBE7", "FFFDE7", "FFF8E1", "FFF3E0",
+        "FBE9E7", "EFEBE9", "FAFAFA", "EFEBE9"
+    ],
+    "material_500":[
+        "F44336","E91E63","9C27B0","673AB7","3F51B5",
+        "2196F3","03A9F4","00BCD4","009688","4CAF50",
+        "8BC34A","CDDC39","FFEB3B","FFC107","FF9800",
+        "FF5722","795548","9E9E9E","607D8B"
+    ],
+    "material_a100": [
+        "FF8A80", "FF80AB", "EA80FC", "B388FF", "8C9EFF",
+        "82B1FF", "80D8FF", "84FFFF", "A7FFEB", "B9F6CA",
+        "CCFF90", "F4FF81", "FFFF8D", "FFE57F", "FFD180",
+        "FF9E80", "D7CCC8", "F5F5F5", "CFD8DC"
+    ]
+}
+function dice(n, s, b) {
+    var r = Math.random();
+    var s_for = 0;
+    var out1 = 0,
+    out2 = 0;
+    for (jj = 0; jj < n; jj++) {
+        for (ii = 0; ii < s; ii++) {
+            s_for++;
+            if (s_for >= s * r) {
+                out1 = s_for;
+                break;
+            }
+        }
+        r = Math.random();
+        s_for = 0;
+        out2 = out2 + out1;
+    }
+    return out2 + b;
+}
+var color_i = dice(1, color.length, 0);
 
 $(function() {
     nav_create();
