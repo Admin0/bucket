a3c50dcc4823be049ddeafbd3366d282902fc33c
JinH <givemebigpie@naver.com>
2016-06-21 16:55:48 +0900

    Update for coloring <nav>
    네비게이션 제목의 색이 스크롤 위치에 따라 아름답게 변함.

--- main.js
+++ main.js
@@ -1,3 +1,92 @@
+color = {
+    "length":19,
+    "material_50":[
+        "#FFEBEE", "#FCE4EC", "#F3E5F5", "#EDE7F6", "#E8EAF6",
+        "#E3F2FD", "#E1F5FE", "#E0F7FA", "#E0F2F1", "#E8F5E9",
+        "#F1F8E9", "#F9FBE7", "#FFFDE7", "#FFF8E1", "#FFF3E0",
+        "#FBE9E7", "#EFEBE9", "#FAFAFA", "#EFEBE9"
+    ],
+    "material_100":[
+        "#FFCDD2", "#F8BBD0", "#E1BEE7", "#D1C4E9", "#C5CAE9",
+        "#BBDEFB", "#B3E5FC", "#B2EBF2", "#B2DFDB", "#C8E6C9",
+        "#DCEDC8", "#F0F4C3", "#FFF9C4", "#FFECB3", "#FFE0B2",
+        "#FFCCBC", "#D7CCC8", "#F5F5F5", "#CFD8DC"
+    ],
+    "material_300":[
+        "#E57373", "#F06292", "#BA68C8", "#9575CD", "#7986CB",
+        "#64B5F6", "#4FC3F7", "#4DD0E1", "#4DB6AC", "#81C784",
+        "#AED581", "#DCE775", "#FFF176", "#FFD54F", "#FFB74D",
+        "#FF8A65", "#A1887F", "#E0E0E0", "#90A4AE"
+    ],
+    "material_500":[
+        "#F44336", "#E91E63", "#9C27B0", "#673AB7", "#3F51B5",
+        "#2196F3", "#03A9F4", "#00BCD4", "#009688", "#4CAF50",
+        "#8BC34A", "#CDDC39", "#FFEB3B", "#FFC107", "#FF9800",
+        "#FF5722", "#795548", "#9E9E9E", "#607D8B"
+    ],
+    "material_600": [
+        "#E53935", "#D81B60", "#8E24AA", "#5E35B1", "#3949AB",
+        "#1E88E5", "#039BE5", "#00ACC1", "#00897B", "#43A047",
+        "#7CB342", "#C0CA33", "#FDD835", "#FFB300", "#FB8C00",
+        "#F4511E", "#6D4C41", "#757575", "#546E7A"
+    ],
+    "material_700":[
+        "#D32F2F", "#C2185B", "#7B1FA2", "#512DA8", "#303F9F",
+        "#1976D2", "#0288D1", "#0097A7", "#00796B", "#388E3C",
+        "#689F38", "#AFB42B", "#FBC02D", "#FFA000", "#F57C00",
+        "#E64A19", "#5D4037", "#616161", "#455A64"
+    ],
+    "material_800": [
+        "#C62828", "#AD1457", "#6A1B9A", "#4527A0", "#283593",
+        "#1565C0", "#0277BD", "#00838F", "#00695C", "#2E7D32",
+        "#558B2F", "#9E9D24", "#F9A825", "#FF8F00", "#EF6C00",
+        "#D84315", "#4E342E", "#424242", "#37474F"
+    ],
+    "material_900":[
+        "#B71C1C", "#880E4F", "#4A148C", "#311B92", "#1A237E",
+        "#0D47A1", "#01579B", "#006064", "#004D40", "#1B5E20",
+        "#33691E", "#827717", "#F57F17", "#FF6F00", "#E65100",
+        "#BF360C", "#3E2723", "#212121", "#263238"
+    ],
+    "material_a100": [
+        "#FF8A80", "#FF80AB", "#EA80FC", "#B388FF", "#8C9EFF",
+        "#82B1FF", "#80D8FF", "#84FFFF", "#A7FFEB", "#B9F6CA",
+        "#CCFF90", "#F4FF81", "#FFFF8D", "#FFE57F", "#FFD180",
+        "#FF9E80", "#D7CCC8", "#F5F5F5", "#CFD8DC"
+    ],
+    "name": [
+        "Red",          "Pink",          "Purple",      "Deep Purple",  "Indigo",
+        "Blue",         "Light Blue",    "Cyan",        "Teal",         "Green",
+        "Light Green",  "Lime",          "Yellow",      "Amber",        "Orange",
+        "Deep Orange",  "Brown",         "Grey",        "Blue Grey"
+    ]
+}
+function dice(n, s, b) {
+    var out = 0;
+    for (i = 0; i < n; i++) {
+        out += Math.ceil( Math.random() * s );
+    }
+    return out + b;
+}
+var color_i = dice(1, color.length, -1);
+console.log("COLOR CODE: " + color.name[color_i]);
+
+function coloring() {
+    if(color_i == 12 /*yellow*/){ color.material_500 = color.material_600; }
+
+    $("header").css({"background":color.material_700[color_i]});
+    $("#sub_header .filter_bt, #column_bt").hover(function(){
+        $(this).css({"background":color.material_700[color_i]});
+    },function(){
+        $(this).css({"background":color.material_500[color_i]});
+    })
+    $("#sub_header > #line1").css({"background":color.material_500[color_i]});
+    $("dl .material-icons, a:not(nav a), #to_github, #to_github > i").css({"color":color.material_500[color_i],"fill":color.material_500[color_i]})
+
+    $("link[rel~='icon']").attr("href", "img/[favicon]/favicon" + color_i + ".ico");
+    $("meta[name='theme-color']").attr("content", color.material_700[color_i]);
+}
+
 function nav_create() {
     // $("body").prepend("<nav><div id='nav_header'></div><div id='nav'></div><div id='nav_footer'><i class='material-icons'>chevron_left</i></div></nav>");
     // $("nav").prepend("<section id='in-page'></section>");
@@ -213,68 +302,6 @@ function title_tooltip(){
     );
 }
 
-color = {
-    "length":15,
-    "material_50":[
-        "#FFEBEE", "#FCE4EC", "#F3E5F5", "#EDE7F6", "#E8EAF6",
-        "#E3F2FD", "#E1F5FE", "#E0F7FA", "#E0F2F1", "#E8F5E9",
-        "#F1F8E9", "#F9FBE7", "#FFFDE7", "#FFF8E1", "#FFF3E0",
-        "#FBE9E7", "#EFEBE9", "#FAFAFA", "#EFEBE9"
-    ],
-    "material_500":[
-        "#F44336", "#E91E63", "#9C27B0", "#673AB7", "#3F51B5",
-        "#2196F3", "#03A9F4", "#00BCD4", "#009688", "#4CAF50",
-        "#8BC34A", "#CDDC39", "#FDD835", "#FFC107", "#FF9800",  //yellow:600
-        "#FF5722", "#795548", "#9E9E9E", "#607D8B"
-    ],
-    "material_700":[
-        "#D32F2F", "#C2185B", "#7B1FA2", "#512DA8", "#303F9F",
-        "#1976D2", "#0288D1", "#0097A7", "#00796B", "#388E3C",
-        "#689F38", "#AFB42B", "#FBC02D", "#FFA000", "#F57C00",
-        "#E64A19", "#5D4037", "#616161", "#455A64"
-    ],
-    "material_a100": [
-        "#FF8A80", "#FF80AB", "#EA80FC", "#B388FF", "#8C9EFF",
-        "#82B1FF", "#80D8FF", "#84FFFF", "#A7FFEB", "#B9F6CA",
-        "#CCFF90", "#F4FF81", "#FFFF8D", "#FFE57F", "#FFD180",
-        "#FF9E80", "#D7CCC8", "#F5F5F5", "#CFD8DC"
-    ]
-}
-function dice(n, s, b) {
-    var r = Math.random();
-    var s_for = 0;
-    var out1 = 0,
-    out2 = 0;
-    for (jj = 0; jj < n; jj++) {
-        for (ii = 0; ii < s; ii++) {
-            s_for++;
-            if (s_for >= s * r) {
-                out1 = s_for;
-                break;
-            }
-        }
-        r = Math.random();
-        s_for = 0;
-        out2 = out2 + out1;
-    }
-    return out2 + b;
-}
-var color_i = dice(1, color.length, 0);
-
-function coloring() {
-    $("header").css({"background":color.material_700[color_i]});
-    $("#sub_header .filter_bt, #column_bt").hover(function(){
-        $(this).css({"background":color.material_700[color_i]});
-    },function(){
-        $(this).css({"background":color.material_500[color_i]});
-    })
-    $("#sub_header > #line1").css({"background":color.material_500[color_i]});
-    $("dl .material-icons, a:not(nav a), #to_github, #to_github > i").css({"color":color.material_500[color_i],"fill":color.material_500[color_i]})
-
-    $("link[rel~='icon']").attr("href", "img/[favicon]/favicon" + color_i + ".ico");
-    $("meta[name='theme-color']").attr("content", color.material_700[color_i]);
-}
-
 function showMovie(src) {
     var url = src.replace("watch?v=", "embed/");
     $("body").append("<div id='movie_wrap'><iframe class='shadow' width='560' height='315' src='" + url + "?rel=0&amp;showinfo=0'  frameborder='0' allowfullscreen></iframe></div>")
@@ -330,7 +357,7 @@ function imgReady() {
             j = $(".img:nth(" + i + ") > div").index(this);
             flex = 100 * obj[j][0] * obj[0][1] / obj[j][1];
             // console.log(i+"번 .img의 "+j+"번째 img 정렬: " + flex);
-            $(this).css( {"flex" : flex } );
+            $(this).css( {"flex" : flex + "%" } );
         });
     });
 }
@@ -550,7 +577,22 @@ function card_wrap(){
 }
 
 function percentage(){
-    $("h1").append("<class class='percentage'>" + ($("input[checked]").length / ($("input").length - $("input[failed]").length) * 100).toFixed(0) + "% (" +
+
+    var percent_total = ($("input[checked]").length / ($("input").length - $("input[failed]").length) * 100).toFixed(0);
+
+    if(percent_total >= 75){ $("#재귀함수_75").attr("checked", true); }
+    if(percent_total >= 50){ $("#재귀함수_50").attr("checked", true); }
+    if(percent_total >= 25){ $("#재귀함수_25").attr("checked", true); }
+
+    if(percent_total >= 75){
+        $("#재귀함수_75 + dt + dd > .date").text("." + percent_total + " 완료");
+    } else if(percent_total >= 50){
+        $("#재귀함수_50 + dt + dd > .date").text("." + percent_total + " 완료");
+    } else if(percent_total >= 25){
+        $("#재귀함수_25 + dt + dd > .date").text("." + percent_total + " 완료");
+    }
+
+    $("h1").append("<class class='percentage'>" + percent_total + "% (" +
     $("input[checked]").length + "/" + ($("input").length - $("input[failed]").length) + ")</span>");
 
     $("h2:not(nav h2)").each(function(){
@@ -693,6 +735,25 @@ function contextmenu() {
         }
     });
 }
+
+$(window).scroll(function(){
+    for(i=$("h3:not(nav h3)").length-1; i>=0; i--){
+        var target = $("h3:not(nav h3):nth("+i+")");
+        // console.log(target.offset().top);
+        if(target.offset().top < pageYOffset + 256){
+            $("nav h3 a").css({"color":"inherit"});
+            $("nav h3:nth("+i+") a").css({"color":color.material_500[color_i]});
+            // console.log(target.text());
+            break;
+        }
+    }
+    // $("h2:not(nav h2), h3:not(nav h3)").each(function(){
+    //     if($(this).offset().top < pageYOffset){
+    //         console.log($(this).text());
+    //     }
+    // });
+    // console.log("sex");
+});
 
 $(window).resize(function() {
     columns();
