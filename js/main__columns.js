bbd20cd7f5283cf5086c875873479dec327de90f
JinH <jinh719@gmail.com>
2019-05-12 02:34:00 +0900

    3.5.04

--- js/main__columns.js
+++ js/main__columns.js
@@ -1,15 +1,8 @@
 var dashboard_top;
 
 function columns_dashboard(type) {
-  if ($(".dashboard").length != 0) {
-    dashboard_top = $(".dashboard").offset().top - 116;
-  } else {
-    $.get("module/dashboard.html", function(data) {
-      $('section#first_class').append(data);
-      coloring();
-      dashboard_top = $(".dashboard").offset().top - 116;
-    });
-  }
+  dashboard_top = $("#그거_먹는건가요").offset().top + 22;
+  //     dashboard_top = $(".dashboard").offset().top - 116;
   if (dashboard_top < pageYOffset && window.localStorage['setting__stat'] == "true") {
     if ($(".wrap_dashboard.floating").length == 1) {
       var b_w = $("body").width();
