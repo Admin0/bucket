627af3c5e463a0bb3edd569d66c010c6a7344496
JinH <jinh719@gmail.com>
2023-11-06 01:14:09 +0900

    3.7.18 다크 모드 개선

--- js/sub__coloring.js
+++ js/sub__coloring.js
@@ -129,4 +129,23 @@ document.documentElement.style.setProperty("--color-a200", color.material_a200[c
 document.documentElement.style.setProperty("--color-a400", color.material_a400[color.i]);
 document.documentElement.style.setProperty("--color-a700", color.material_a700[color.i]);
 
+const getTextColorByBackgroundColor = (hexColor) => {
+  const c = hexColor.substring(1) // 색상 앞의 # 제거
+  const rgb = parseInt(c, 16) // rrggbb를 10진수로 변환
+  const r = (rgb >> 16) & 0xff // red 추출
+  const g = (rgb >> 8) & 0xff // green 추출
+  const b = (rgb >> 0) & 0xff // blue 추출
+  const luma = 0.2126 * r + 0.7152 * g + 0.0722 * b // per ITU-R BT.709
+  // 색상 선택
+ if (luma < 200) {
+  document.documentElement.style.setProperty("--color-dark-tooltip", "rgba(255, 255, 255, .75)");
+ } else {
+  document.documentElement.style.setProperty("--color-dark-tooltip", "rgba(0, 0, 0, .85)");
+ }
+  // const result = luma < 127.5 ? 'white' : 'black'
+  // console.log(luma)
+  // return result // 글자색이
+}
+getTextColorByBackgroundColor(color.material_a200[color.i])
+
 // time.log('coloring done');
